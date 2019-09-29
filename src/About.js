069810da258d30dd51d3aa5e060525ed55c19547
Alex Shellim <alexshellim@alexs-mbp.home>
2019-09-29 21:07:44 +0100

    first commit

--- src/About.js
+++ src/About.js
@@ -4,7 +4,8 @@ function About(props) {
   return (
     <div>
       <h1>About me</h1>
-      <p>I'm Alex.</p>
+      <p>I'm Alex. I'm a (product) designer. I believe in creating intuitive experiences that solve problems and deliver value through research, insight and pixel-perfect design.</p>
+      <p>I currently work as part of Sainsbury's Experience Design team but I've previously spend time at an insurtech start-up and at Songkick.com designing and developing campaigns for some of the world's biggest artists.</p>
     </div>
   )
 }
--- src/Work.js
+++ src/Work.js
@@ -3,7 +3,7 @@ import React from "react"
 function Work(props) {
   return (
     <div>
-      <h1>Work</h1>
+      <h1>My work</h1>
     </div>
   )
 }
