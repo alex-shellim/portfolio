74b7f53b4adf7207c8abbe62a83412f7ea49c397
Alex Shellim <alexshellim@gmail.com>
2019-10-11 16:58:56 +0100

    about me

--- src/About.js
+++ src/About.js
@@ -4,8 +4,9 @@ function About(props) {
   return (
     <div>
       <h1>About me</h1>
-      <p>I'm Alex. I'm a (product) designer. I believe in creating intuitive experiences that solve problems and deliver value through research, insight and pixel-perfect design.</p>
-      <p>I currently work as part of Sainsbury's Experience Design team but I've previously spend time at an insurtech start-up and at Songkick.com designing and developing campaigns for some of the world's biggest artists.</p>
+      <p>I'm Alex. I'm a London-based (product) designer. I'm passionate about creating intuitive, accessible experiences that solve problems and deliver value through research, insight and visual design.</p>
+      <p>I currently work as part of Sainsbury's Experience Design team on their Groceries Online product. Previously I spent time at an insurtech start-up and at Songkick.com designing and developing campaigns for some of the world's biggest artists.</p>
+      <p>I like noisy music and baking my own bread.</p>
     </div>
   )
 }
