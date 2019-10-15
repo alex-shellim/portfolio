909f01de9d6b2081049a28d328e9059727bba77e
Alex Shellim <alex.shellim@gmail.com>
2019-10-15 22:20:51 +0100

    updates

--- src/Work.js
+++ src/Work.js
@@ -7,18 +7,26 @@ function Work(props) {
     // Render a Thumbnail component
     <div>
       <h1>Work</h1>
-      <Thumbnail
-        link="/Songkick"
-        image="https://images.sk-static.com/images/media/profile_images/artists/3277856/huge_avatar"
-        title="Songkick"
-        category="Web Design"
-      />
-      <Thumbnail
-        link="/Sainsburys"
-        image="https://144f2a3a2f948f23fc61-ca525f0a2beaec3e91ca498facd51f15.ssl.cf3.rackcdn.com/uploads/food_portal_data/recipes/recipe/hero_article_image/4047/ef55c0c8a64cfb43f4d5/letterbox_Sainsburys_MacBrocolli3.jpg"
-        title="Sainsbury's Groceries Online"
-        category="Product Design"
-      />
+      <div class="thumbnail-gallery">
+        <Thumbnail
+          link="/Songkick"
+          image="https://images.sk-static.com/images/media/profile_images/artists/3277856/huge_avatar"
+          title="Songkick"
+          category="Web Design"
+        />
+        <Thumbnail
+          link="/Songkick"
+          image="https://images.sk-static.com/images/media/profile_images/artists/3277856/huge_avatar"
+          title="Songkick"
+          category="Web Design"
+        />
+        <Thumbnail
+          link="/Songkick"
+          image="https://images.sk-static.com/images/media/profile_images/artists/3277856/huge_avatar"
+          title="Songkick"
+          category="Web Design"
+        />
+      </div>
     </div>
   )
 }
